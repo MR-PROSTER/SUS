4c5d440977978ad06f7f09b44cfe0d07267cb13b
M PRANEETH <madasu.praneeth464@gmail.com>
2026-02-10 05:08:22 +0530

    Final Working....

--- Frontend/src/Components/Body.tsx
+++ Frontend/src/Components/Body.tsx
@@ -1,5 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, {  useState } from 'react'
 import Upload from './Upload'
+import img1 from '../assets/images/img1.png'
+import img2 from '../assets/images/img2.png'
+import img3 from '../assets/images/img3.png'
+import img4 from '../assets/images/img4.png'
 
 const Body = () => {
   const [file, setFile] = useState<File | null>(null)
@@ -64,7 +68,7 @@ const Body = () => {
     <>
     <div className='min-h-screen w-full flex flex-col items-center justify-center gap-24'>
         <h1 className='font-peanutButter text-8xl text-[#F1E3E4]'>Effortless Compression. Instant Results.</h1>
-        <div className='min-h-80 min-w-[640px]  border-[1px] border-[#F1E3E4] outline-dashed outline-offset-8 outline-[#F1E3E4] flex items-center justify-center'>
+        <div className='min-h-80 min-w-[640px] bg-[#CCBCBC]  border-[1px] border-[#F1E3E4] outline-dashed outline-offset-8 outline-[#F1E3E4] flex items-center justify-center'>
 <Upload
   onFileSelect={ async (file) => {
     if (!file) return;
@@ -73,7 +77,53 @@ const Body = () => {
     sendFileData(file.name);
   }}
 />
+
+    </div>
+        <h1 className='font-peanutButter text-8xl text-[#F1E3E4] mt-24'>Shrink PDF Size, Preserve Quality</h1>
+    </div>
+    <div className='flex flex-col items-center justify-center -translate-y-16'>
+    <p className='text-[#F1E3E4] text-2xl'>Compress PDFs online for free, maintaining document quality. Reduce large files for</p>
+    <span className='text-[#F1E3E4] text-2xl'> easy sharing and storage in just a few clicks.</span>
+    </div>
+    <div className='flex flex-col items-center justify-center gap-12 mt-6'>
+     {/* Features Section */}
+     {/* Feature 1 */}
+    <div className='min-h-[500px]  w-[1200px] flex items-center justify-center'>
+        <div className='h-[400px] w-[600px]  mt-16'>
+        <h1 className='font-molot text-[#F1E3E4] text-4xl mx-6 '>Fast & Effective Compression</h1>
+        <p className=' text-[#F1E3E4] text-xl mx-6 mt-24'>Compress your PDF effortlessly and instantly. Simply drag and drop your file; our tool will automatically optimize it within seconds. Enjoy seamless online performance across all major platforms: Mac, Windows, Linux, iOS, and Android. No manual adjustments needed.</p>
         </div>
+        <img src={img1} alt="Sample PDF" className='h-[400px] w-[600px] scale-125'/>
+    </div>
+    {/* Feature 2 */}
+        <div className='min-h-[500px]  w-[1200px] flex items-center justify-center'>
+        <img src={img2} alt="Sample PDF" className='h-[400px] w-[600px] scale-125'/>
+        <div className='h-[400px] w-[600px]  mt-16'>
+        <h1 className='font-molot text-[#F1E3E4] text-4xl mx-6 '>Smaller PDFs, Same High Quality</h1>
+        <p className=' text-[#F1E3E4] text-xl mx-6 mt-24'>Select between Basic and Strong PDF compression for the optimal balance. Reduce file size without noticeable quality loss.</p>
+        </div>
+    </div>
+    {/* Feature 3 */}
+        <div className='min-h-[500px]  w-[1200px] flex items-center justify-center'>
+        <div className='h-[400px] w-[600px]  mt-16'>
+        <h1 className='font-molot text-[#F1E3E4] text-4xl mx-6 '>Secure Compression, Trusted Worldwide</h1>
+        <p className=' text-[#F1E3E4] text-xl mx-6 mt-24'>Your documents are secured with TLS encryption, adhere to GDPR, and are ISO/IEC 27001 certified. For your privacy, we automatically delete files after one hour.</p>
+        </div>
+        <img src={img3} alt="Sample PDF" className='h-[400px] w-[600px] scale-125'/>
+    </div>
+    {/* Feature 4 */}
+     <div className='min-h-[500px]  w-[1200px] flex items-center justify-center bg-[#141519]'>
+        <img src={img4} alt="Sample PDF" className='h-[400px] w-[600px] scale-150'/>
+        <div className='h-[400px] w-[600px]  mt-16'>
+        <h1 className='font-molot text-[#F1E3E4] text-4xl mx-6 '>How To Compress a PDF Online for Free</h1>
+        <p className=' text-[#F1E3E4] text-xl mx-6 mt-16'>1. Drag & drop your file into our free PDF Compressor tool.</p>
+        <p className=' text-[#F1E3E4] text-xl mx-6 mt-4'>2. Choose Basic or Strong (Pro) compression, then click ‘Compress’.</p>
+        <p className=' text-[#F1E3E4] text-xl mx-6 mt-4'>3. If needed, edit your PDF with our other tools.</p>
+        <p className=' text-[#F1E3E4] text-xl mx-6 mt-4'>4. Download or share your compressed file when you’re done.
+        </p>
+        </div>
+    </div>
+    <div className="min-h-32"></div>
     </div>
     </>
   )
